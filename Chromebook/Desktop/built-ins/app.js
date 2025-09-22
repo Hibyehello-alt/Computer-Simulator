7d70186940d965e5222d7f9bf83789f46c908c41
Hibyehello-alt <203482278+Hibyehello-alt@users.noreply.github.com>
2025-09-22 02:51:50 +0000

    Allow Focus while dragging

--- Chromebook/Desktop/built-ins/app.js
+++ Chromebook/Desktop/built-ins/app.js
@@ -30,7 +30,7 @@ export class BaseApp {
         document.body.appendChild(this.appDiv);
 
         if(this.appHeader && this.appHeader.onmousedown == null) { 
-            dragElement(this.appDiv);
+            this.EnableDragging();
         }
         this.focusApp();
         this.appDiv.addEventListener("click", this.focusApp);
@@ -158,5 +158,46 @@ export class BaseApp {
 
     addDesktopMenuIcon() {
         document.getElementById("desktopMenu-Center").appendChild(this.small_icon);
+    }
+
+    EnableDragging() {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (this.appHeader) {
+            this.appHeader.onmousedown = dragMouseDown;
+        }
+
+        let elmnt = this.appDiv;
+        let parent = this;
+
+        function dragMouseDown(e) {
+            parent.focusApp();
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
     }
 }
--- Chromebook/Desktop/script.js
+++ Chromebook/Desktop/script.js
@@ -3,7 +3,6 @@ var installedApps = {};
 /* Main javascript (onload Events) */
 async function main() {
   setCurrentWallpaper();
-  dragElement(document.getElementById("devConsole"));
   menuMore();
   if(true) {
     document.getElementById('settings-user').innerHTML = 'HelloWorld';
@@ -667,7 +666,7 @@ function devConsoleInput() {
     const regex = /\("([^"]+)"\)/;
     let id = devInput.match(regex);
     console.log(id[1]);
-    dragElement(document.getElementById(id[1]));
+    installedApps[id[1]].EnableDragging();
     devCommand();
   } else if(devInput == "#currentImg") {
     devVar(currentImg);
@@ -754,49 +753,4 @@ function getCookie(cname) {
     }
   }
   return "";
-}
-
-var dragging = false;
-
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    console.log(elmnt.id + "-header")
-    if (document.getElementById(elmnt.id + "-header")) {
-      // if present, the header is where you move the DIV from:
-      document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
-    } // else {
-    //   // otherwise, move the DIV from anywhere inside the DIV:
-    //   elmnt.onmousedown = dragMouseDown;
-    // }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    dragging = true;
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
 }
